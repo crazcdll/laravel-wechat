9d783f973c0f7e4faf936f954304f800780ec00c
zcdll <qinsword521@gmail.com>
2017-07-24 00:30:40 +0800

    去掉 log

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Log;
+//use Log;
 
 use Illuminate\Http\Request;
 
@@ -15,14 +15,14 @@ class WechatController extends Controller
      */
     public function serve()
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+//        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
             return "try laravel-wechat";
         });
 
-        Log::info('return response.');
+//        Log::info('return response.');
 
         return $wechat->server->serve();
     }
